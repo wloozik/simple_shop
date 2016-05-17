835de8d03284176ec6b2a2f4ab04800f83977ca2
wloozik <wloozig@gmail.com>
2016-05-17 23:44:42 +0500

    OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Admin.create(email: 'admin@simle-shop.com', password: 'moonlight') unless Admin.find_by(email: 'admin@simle-shop.com').present?
+Admin.create(email: 'wloozig@gmail.com', password: '3agrutrika') unless Admin.find_by(email: 'wloozig@gmail.com').present?
