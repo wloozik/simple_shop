1b20e15592bdfac5a5b74578b5c8c8ba65ccd76f
wloozik <wloozig@gmail.com>
2016-05-18 03:08:55 +0500

    3hours

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'immense-island-19716.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'efremova.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address    => "smtp.sendgrid.net",
     :port       => 25,
