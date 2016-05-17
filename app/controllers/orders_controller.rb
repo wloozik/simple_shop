ea60dfc9b6080d4393a6e82f8827675eabefd23f
twinfacer <twinfacer@gmail.com>
2016-05-18 04:35:53 +0500

    Orders & Users stuff

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,14 +8,15 @@ class OrdersController < ApplicationController
     if @cart.present? && @cart.cart_items.any?
       order_items = @cart.cart_items.map { |ci| {  product_id: ci.product.id, quantity: ci.quantity } }
       # Создали заказ из содержимого корзины
-      Order.create(
+      @order = Order.create(
         user: current_user,
-        status: :created, 
+        status: :created,
         order_items_attributes: order_items
       )
       # Очистили корзину
       @cart.cart_items.destroy_all
       # Вернули на главную c сообщением что заказ создан
+      UsersMailer.order(current_user, @order).deliver_now!
       redirect_to root_path, flash: { notice: 'Заказ оформлен' }
     else
       # Иначе просто вернули на главную
---
+++ db/migrate/20160517225208_add_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :address, :string
+    add_column :users, :phone, :string
+    add_column :users, :name, :string
+  end
+end
